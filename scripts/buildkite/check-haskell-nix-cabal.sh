89e35fabf5073ed647cabcf0bcf69ce9df1624c7
Samuel Evans-Powell <mail@sevanspowell.net>
2022-04-04 13:49:02 +0800

    Test hie-bios invocation

--- scripts/buildkite/check-haskell-nix-cabal.sh
+++ scripts/buildkite/check-haskell-nix-cabal.sh
@@ -16,5 +16,10 @@ echo
 
 echo "+++ haskell-language-server"
 ln -sf hie-direnv.yaml hie.yaml
+# hie-bios occasionally segfaults. Re-running is usually enough to overcome the
+# segfault.
+hie-bios check lib/core/src/Cardano/Wallet.hs || true
+hie-bios check lib/core/src/Cardano/Wallet.hs
 haskell-language-server lib/core/src/Cardano/Wallet.hs
+haskell-language-server lib/shelley/exe/cardano-wallet.hs
 echo
