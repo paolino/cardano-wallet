e6ef7a1c9a286d4c75966475ab0bd67bf11a6089
Samuel Evans-Powell <mail@sevanspowell.net>
2022-04-04 16:20:06 +0800

    Ensure hie-bios & hs-language-server is available in Buildkite step

--- nix/cabal-shell.nix
+++ nix/cabal-shell.nix
@@ -30,6 +30,8 @@ mkShell rec {
     nixWrapped
     pkgconfig
     gnutar
+    haskell-build-tools.hie-bios
+    haskell-build-tools.haskell-language-server
   ]
   ++ lib.optional (!stdenv.isDarwin) git
   ++ (with haskellProject.hsPkgs; [
